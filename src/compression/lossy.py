79b3e7051c5fae7dab4f3ef5225b67e0f862abb7
leonard audibert <leonard.audibert@gmail.com>
2020-07-13 09:03:39 -0700

    Test YCbCr splitting

--- src/compression/lossy.py
+++ src/compression/lossy.py
@@ -1,22 +1,48 @@
 from typing import List
+import logging
+
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
 
 # TODO: implement JPEG
 # TODO: implement GMM
 
 
-def downsample(image: List[List[int]]):
+def downsample(image: List[List[List[int]]]):
     pass
 
 
-def JPEG(image):
-    layers = separate_image_layers(image)
+def JPEG(image: List[List[List[int]]]) -> List[List[List[int]]]:
+    """Encode in JPEG-like format and return the decoded encoded image
+    """
+    # Separate into Y, Cb, Cr layers
+    layers: List[List[List[int]]] = separate_image_layers(image)
 
+    # Split into 8x8 blocks
     block_split_layers = []
     for layer in layers:
         block_split_layers.append(block_split_layer(layer))
 
+        # Call DCT on every block
+        # Quantize every block
+        # Zigzag entropy code
 
-def separate_image_layers(image: List[List[int]]) -> List[List[List[int]]]:
+        # Save file
+
+        # De-encode Huffman
+        # Dequantize every block
+        # Call DCT inverse on every block
+
+        # Join 8x8 blocks
+        block_join_layer()
+
+    # Join Y, Cb, Cr layers
+    image: List[List[List[int]]] = join_image_layers(layers)
+
+    return image
+
+
+def separate_image_layers(image: List[List[List[int]]]) -> List[List[List[int]]]:
     """Divide the in Y, Cb and Cr layers
     """
     # Split the Y, Cb and Cr layers into 8x8 pixel blocks
@@ -25,6 +51,7 @@ def separate_image_layers(image: List[List[int]]) -> List[List[List[int]]]:
     Cr_layer = []
 
     for row in image:
+        log.debug(row)
         Y_layer_row = []
         Cb_layer_row = []
         Cr_layer_row = []
@@ -39,6 +66,21 @@ def separate_image_layers(image: List[List[int]]) -> List[List[List[int]]]:
         Cr_layer.append(Cr_layer_row)
 
     return [Y_layer, Cb_layer, Cr_layer]
+
+
+def join_image_layers(layers: List[List[List[int]]]) -> List[List[List[int]]]:
+    """Join the Y, Cb, Cr layers into a single image
+    """
+    # Create image matrix with the same number of rows as there are in a layer.
+    # For each row, add a list for every value in the layer's row. This is for (Y,Cb, Cr).
+    image = [[[] for j in len(layers[0][0])] for i in len(layers[0])]
+
+    for layer in layers:
+        for row_index, row in enumerate(layer):
+            for col_index, col in enumerate(row):
+                image[row_index][col_index].append(col)
+
+    return image
 
 
 def block_split_layer(layer: List[List[int]]):
@@ -152,8 +194,3 @@ def block_join_layer(blocked_layer: List[List[int]]):
             layer.append(row)
 
     return layer
-
-
-# Call DCT on every block
-# Quantize every block
-# Zigzag entropy code
--- src/test/compression/test_lossy.py
+++ src/test/compression/test_lossy.py
@@ -7,7 +7,7 @@ log.setLevel(logging.DEBUG)
 
 #########################################
 #                                       #
-# Block splitting                       #
+# Block splitting & joining             #
 #                                       #
 #########################################
 
@@ -64,7 +64,6 @@ test_block_split = [
             [1, 1, 1, 1, 1, 1, 1, 1],
         ],
         expected_output=[
-            # The 8x8 square of values become a "single value" at (0, 0)
             [
                 [
                     [1, 1, 1, 1, 1, 1, 1, 1],
@@ -104,11 +103,11 @@ def test_block_splitting(case):
     log.debug("Result: " + str(result))
     assert(result == case.expected_output)
 
+
 test_block_join = [
     InputOutputCase(
         name="Nominal",
-        input_val =[
-            # The 8x8 square of values become a "single value" at (0, 0)
+        input_val=[
             [
                 [
                     [1, 1, 1, 1, 1, 1, 1, 1],
@@ -122,7 +121,7 @@ test_block_join = [
                 ]
             ]
         ],
-        expected_output =[
+        expected_output=[
             [1, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 1],
             [1, 1, 1, 1, 1, 1, 1, 1],
@@ -135,17 +134,86 @@ test_block_join = [
     ),
 ]
 
+
 @pytest.mark.parametrize("case", test_block_join)
 def test_block_joining(case):
     """This test is the opposite of test_block_splitting and
     uses the same test data, except reversed.
     """
     log.info("Case: " + case.name)
-
-
     log.debug("Input: " + str(case.input_val))
 
     result = block_join_layer(case.input_val)
+
+    log.debug("Result: " + str(result))
+    assert(result == case.expected_output)
+
+#########################################
+#                                       #
+# YCbCr splitting & joining             #
+#                                       #
+#########################################
+
+
+test_YCbCr_split = [
+    InputOutputCase(
+        name="Nominal",
+        input_val=[
+            [[1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1]]
+        ],
+        expected_output=[
+            [
+                [1, 1],
+                [1, 1]
+            ],
+            [
+                [1, 1],
+                [1, 1]
+            ],
+            [
+                [1, 1],
+                [1, 1]
+            ]
+        ]
+    ),
+    InputOutputCase(
+        name="3x4 iamges",
+        input_val=[
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]],
+            [[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]],
+        ],
+        expected_output=[
+            [
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+            ],
+            [
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+            ],
+            [
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+                [1, 1, 1, 1],
+            ]
+        ]
+    ),
+]
+
+
+@pytest.mark.parametrize("case", test_YCbCr_split)
+def test_YCbCr_splitting(case):
+    """This test is the opposite of test_block_splitting and
+    uses the same test data, except reversed.
+    """
+    log.info("Case: " + case.name)
+    log.debug("Input: " + str(case.input_val))
+
+    result = separate_image_layers(case.input_val)
 
     log.debug("Result: " + str(result))
     assert(result == case.expected_output)
